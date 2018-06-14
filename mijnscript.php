ef07192faa8e80e6b2ab48696eebdf90e4c5f0ea
JasGri <38950869+JasGri@users.noreply.github.com>
2018-06-14 16:47:23 +0200

    Add files via upload

--- mijnscript.php
+++ mijnscript.php
@@ -1,9 +1,18 @@
 <?php
+
+$datum = "Datum: " . date("Y/m/d");
 $blogtitel = $_REQUEST["blogtitel"];
 $naam = $_REQUEST["naam"];
 $posttxt = $_REQUEST["posttxt"];
-file_put_contents("data/blog.txt",$blogtitel . "\r\n",FILE_APPEND);
-file_put_contents("data/blog.txt",$naam . "\r\n",FILE_APPEND);
-file_put_contents("data/blog.txt",$posttxt . "\r\n",FILE_APPEND);
-$message =  "Je blog is geplaatst!";
-echo "<script type='text/javascript'>alert('$message');</script>";
+file_put_contents("data/datum.txt",$datum . "\r\n\r\n",FILE_APPEND);
+file_put_contents("data/blogtitel.txt",$blogtitel . "\r\n\r\n",FILE_APPEND);
+file_put_contents("data/naam.txt",$naam . "\r\n\r\n",FILE_APPEND);
+file_put_contents("data/posttxt.txt",$posttxt . "\r\n\r\n",FILE_APPEND);
+
+$reverse=array_reverse($datum);
+$reverse=array_reverse($blogtitel);
+$reverse=array_reverse($naam);
+$reverse=array_reverse($posttxt);
+
+header("location:/blog/index1.php");
+?>
