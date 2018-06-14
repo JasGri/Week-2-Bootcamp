1397bfaa19e1dae048155202043b01e18dfc3a3f
JasGri <38950869+JasGri@users.noreply.github.com>
2018-06-14 08:03:37 +0200

    Add files via upload

--- mijnscript.php
+++ mijnscript.php
@@ -1,9 +1,9 @@
 <?php
-	function postArticle()
-	{
-			$blogtitel = $_REQUEST["blogtitel"];
-			$naam = $_REQUEST["naam"];
-		  $posttxt = $_REQUEST["posttxt"];
-			file_put_content("/var/www/html/blog/data/post.txt" FILE_APPEND | LOCK_EX);
-		}
-?>
+$blogtitel = $_REQUEST["blogtitel"];
+$naam = $_REQUEST["naam"];
+$posttxt = $_REQUEST["posttxt"];
+file_put_contents("data/blog.txt",$blogtitel . "\r\n",FILE_APPEND);
+file_put_contents("data/blog.txt",$naam . "\r\n",FILE_APPEND);
+file_put_contents("data/blog.txt",$posttxt . "\r\n",FILE_APPEND);
+$message =  "Je blog is geplaatst!";
+echo "<script type='text/javascript'>alert('$message');</script>";
